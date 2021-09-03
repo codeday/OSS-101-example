a0ee76b9af4103967c5f450db1ce3dd71ca2cf0b
A_SCHMITZ <schmitzandrew03@gmail.com>
2021-09-03 13:26:03 -0700

    Add dictionary code

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+word = input("Enter a word: ")
+
+if word == "a":
+    print("one; any")
+elif word == "apple":
+    print("familiar, round fleshy fruit")
+elif word == "rhinoceros":
+    print("large thick-skinned animal with one or two horns on its nose")
+else:
+    print("That word must not exist. This dictionary is very comprehensive.")
